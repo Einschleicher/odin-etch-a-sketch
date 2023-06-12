420c651985067c5939e29cb80351ea6b7acc7dd7
Einschleicher <116537717+Einschleicher@users.noreply.github.com>
2023-06-12 20:05:09 +0200

    Add gridsize button and function to draw new grid

--- script.js
+++ script.js
@@ -6,10 +6,38 @@ for (let i = 0; i < 16 * 16; i++) {
     container.appendChild(square);
 }
 
-const squares = document.querySelectorAll(".square");
+let squares = document.querySelectorAll(".square");
 
 squares.forEach(square => {
     square.addEventListener("mouseover", function(e) {
         square.style.backgroundColor = "black";
+    })
+})
+
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", function() {
+    let userGridSize = prompt("Choose a gridsize (max 100)");
+    while (userGridSize > 100) {
+        userGridSize = prompt("Choose a gridsize (max 100)");
+        }
+    squares.forEach(square => {
+        container.removeChild(square);
+    })
+
+    for (let i = 0; i < userGridSize * userGridSize; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        container.appendChild(square);
+        square.style.width = (800 - 32) / userGridSize + "px";
+        square.style.height = (800 - 32) / userGridSize + "px";
+    }
+
+    squares = document.querySelectorAll(".square");
+
+    squares.forEach(square => {
+        square.addEventListener("mouseover", function(e) {
+            square.style.backgroundColor = "black";
+        })
     })
 })
